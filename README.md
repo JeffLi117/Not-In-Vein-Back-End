6d0058eeba27d94380c66109363931158f338f53
Tomomi-K1 <tomomi.music@gmail.com>
2023-10-17 11:37:29 -0700

    sendgrid test

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # Not-In-Vein-Back-End
+## useful info
+- [setting up sendgrid API](https://docs.sendgrid.com/ui/account-and-settings/api-keys#creating-an-api-key) 
+- [freeCodeCamp article](https://www.freecodecamp.org/news/what-is-sendgrid-smpt-email-newsletter-tutorial/)
--- app.js
+++ app.js
@@ -2,30 +2,48 @@ const { initializeApp, applicationDefault, cert, getApps } = require('firebase-a
 const { getFirestore, Timestamp, FieldValue, Filter } = require('firebase-admin/firestore');
 require('dotenv').config();
 const brevo = require('@getbrevo/brevo');
+// delete this before pushing it
+const serviceAccount = require('./not-in-vein-firebase-adminsdk-6wc6q-5db000729a.json');
 
 const app = initializeApp({
-    credential: cert(process.env.GOOGLE_APPLICATION_CREDENTIALS),
+    credential: cert(serviceAccount),
 });
 
 const db = getFirestore();
 
-let defaultClient = brevo.ApiClient.instance;
+const sgMail = require('@sendgrid/mail');
+const apiKey = process.env.SENDGRID_API_KEY;
+sgMail.setApiKey(apiKey);
+const msg = {
+    to: '',
+    from: 'tomomi.n.kosaka@gmail.com', // Use the email address or domain you verified above
+    subject: 'Sending with Twilio SendGrid is Fun by Tomomi for Test',
+    text: 'this is test to see if SendGrid is working',
+    html: '<strong>and easy to do anywhere, even with Node.js</strong>',
+  };
 
-let apiKey = defaultClient.authentications['api-key'];
-apiKey.apiKey = process.env.API_KEY;
 
-let apiInstance = new brevo.TransactionalEmailsApi();
-let sendSmtpEmail = new brevo.SendSmtpEmail();
 
-sendSmtpEmail.subject = "My {{params.subject}}";
-sendSmtpEmail.htmlContent = "<html><body><h1>Common: This is my first transactional email {{params.parameter}}</h1></body></html>";
-sendSmtpEmail.sender = { "name": "Jeffrey Li - Not In Vein", "email": "jeffrey.t.li.work@gmail.com" };
-sendSmtpEmail.to = [
-  { "email": "{{params.EMAIL}}", "name": "{{params.FULLNAME}}" }
-];
-sendSmtpEmail.replyTo = { "email": "example@brevo.com", "name": "sample-name" };
-sendSmtpEmail.headers = { "Some-Custom-Name": "unique-id-1234" };
-sendSmtpEmail.params = { "parameter": "TESTING", "subject": "Testing subject" };
+// let defaultClient = brevo.ApiClient.instance;
+
+// let apiKey = defaultClient.authentications['api-key'];
+// apiKey.apiKey = process.env.API_KEY;
+
+// let partnerKey = defaultClient.authentications['partner-key'];
+// partnerKey.apiKey = process.env.API_KEY;
+
+// let apiInstance = new brevo.TransactionalEmailsApi();
+// let sendSmtpEmail = new brevo.SendSmtpEmail();
+
+// sendSmtpEmail.subject = "My {{params.subject}}";
+// sendSmtpEmail.htmlContent = "<html><body><h1>Common: This is my first transactional email {{params.parameter}}</h1></body></html>";
+// sendSmtpEmail.sender = { "name": "Jeffrey Li - Not In Vein", "email": "jeffrey.t.li.work@gmail.com" };
+// sendSmtpEmail.to = [
+//   { "email": "{{params.EMAIL}}", "name": "{{params.FULLNAME}}" }
+// ];
+// sendSmtpEmail.replyTo = { "email": "example@brevo.com", "name": "sample-name" };
+// sendSmtpEmail.headers = { "Some-Custom-Name": "unique-id-1234" };
+// sendSmtpEmail.params = { "parameter": "TESTING", "subject": "Testing subject" };
 
 const testFunction = async () => {
     const userRef = db.collection('users').doc('0cse2xite0MLhgYks2K8dJp4XK32');
@@ -34,19 +52,40 @@ const testFunction = async () => {
         console.log('No such document!');
     } else {
         // console.log('Document data:', doc.data());
-        console.log(sendSmtpEmail.params);
-        sendSmtpEmail.params['EMAIL'] = doc.data().email;
-        sendSmtpEmail.params['FULLNAME'] = doc.data().name;
+        // console.log(sendSmtpEmail.params);
+        // sendSmtpEmail.params['EMAIL'] = doc.data().email;
+        // sendSmtpEmail.params['FULLNAME'] = doc.data().name;
+        // console.log(`sendSmtpEmail:`, sendSmtpEmail);
+        msg.to = doc.data().email;
+        console.log(msg);
+        sgMail
+            .send(msg)
+            .then(() => {}, error => {
+                console.error(error);
+
+                if (error.response) {
+                    console.error(error.response.body)
+                }
+            });  
     }
 }
 
-testFunction()
-    .then(() => {
-        console.log(sendSmtpEmail.params);
-        // send the info
-        apiInstance.sendTransacEmail(sendSmtpEmail).then(function (data) {
-            console.log('API called successfully. Returned data: ' + JSON.stringify(data));
-        }, function (error) {
-            console.error(error);
-        });
-    })
+testFunction();
+
+
+
+
+  //ES6
+
+  
+
+// testFunction()
+//     .then(() => {
+//         console.log(sendSmtpEmail.params);
+//         // send the info
+//         apiInstance.sendTransacEmail(sendSmtpEmail).then(function (data) {
+//             console.log('API called successfully. Returned data: ' + JSON.stringify(data));
+//         }, function (error) {
+//             console.error(error);
+//         });
+//     })
